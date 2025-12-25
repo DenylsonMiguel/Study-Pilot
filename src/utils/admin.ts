f7d7e2a14b77637aa7575557277d85c1989ad3e7
DenylsonMiguel <denylsongpereira@gmail.com>
2025-12-25 07:55:03 -0300

    refactor(admin): move admin check to helper function

--- src/utils/admin.ts
+++ src/utils/admin.ts
@@ -1,4 +1,4 @@
-import { User } from "../models/users.js";
+import type { User } from "../models/users.js";
 import { UserRole } from "../modules/auth/auth.roles.js";
 
 export const isAdmin = (user: User): boolean => !user || !user.role || (user.role !== UserRole.ADMIN && user.role !== UserRole.SEED_ADMIN);
